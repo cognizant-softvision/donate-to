a9da4a5d5f3a276edb9879c69a4243a382656955
Miguel Benedit <miguell.beneditt@gmail.com>
2020-04-22 14:51:49 -0300

    Add Entitites Add Interfaces

---
+++ src/DonateTo.ApplicationCore/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DonateTo.ApplicationCore.Entities
+{
+    public abstract class Entity
+    {
+        public long Id { get; set; }
+        public User CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public User UpdateBy { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ src/DonateTo.ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DonateTo.ApplicationCore.Entities;
+
+namespace DonateTo.ApplicationCore.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        IEnumerable<TEntity> Get();
+        
+        Task<IEnumerable<TEntity>> GetAsync();
+
+        TEntity Get(long id);
+
+        Task<TEntity> GetAsync(long id);
+
+        TEntity Add(TEntity entity);
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+    }
+}
