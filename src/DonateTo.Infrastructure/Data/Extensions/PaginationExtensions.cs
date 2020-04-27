6b238cc779ef548ec30ed8b6507a421b97147df2
Matias Grieben <matigrieben@gmail.com>
2020-04-27 15:35:30 -0300

    Added pagination implementation

---
+++ src/DonateTo.Infrastructure/Data/Extensions/PaginationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DonateTo.Infrastructure.Data.Pagination;
+using System;
+using System.Linq;
+
+namespace DonateTo.Infrastructure.Data.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static PagedResult<T> GetPaged<T>(this IQueryable<T> query,
+                                         int page, int pageSize) where T : class
+        {
+            var result = new PagedResult<T>
+            {
+                CurrentPage = page,
+                PageSize = pageSize,
+                RowCount = query.Count()
+            };
+
+            var pageCount = (double)result.RowCount / pageSize;
+            result.PageCount = (int)Math.Ceiling(pageCount);
+
+            var skip = (page - 1) * pageSize;
+            result.SetResults(query.Skip(skip).Take(pageSize).ToList());
+
+            return result;
+        }
+    }
+}
---
+++ src/DonateTo.Infrastructure/Data/Pagination/PagedResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DonateTo.Infrastructure.Data.Pagination
+{
+    public class PagedResult<T> : PagedResultBase where T : class
+    {
+        private IList<T> results;
+
+        public IList<T> GetResults()
+        {
+            return results;
+        }
+
+        public void SetResults(IList<T> value)
+        {
+            results = value;
+        }
+
+        public PagedResult()
+        {
+            SetResults(new List<T>());
+        }
+    }
+}
