217151a6749e5df7c3560ad1ad3f21b56f16cdb4
Karen <31966611+oonosan@users.noreply.github.com>
2020-04-27 17:13:43 -0300

    Create angular solution and add Ant Design (#22)
    * Changing package.json
    
    * Updating Angular to the latest version, adding ant design package
    
    * Added folders and modified app.module
    
    * Added store folders
    
    * Added ngrx dependencies
    
    Co-authored-by: nicolasgranata <nicolasegranata@gmail.com>

--- src/DonateTo.WebApplication/ClientApp/src/app/app.module.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+// Angular core modules
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+// Modules
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+// Third party libraries
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent
@@ -26,7 +31,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
