af84edc27b521c04efc4ad1f85c58019184ee2c8
Nico Granata <nicolasegranata@gmail.com>
2020-04-28 15:30:49 -0300

    Adding summary to the existing methods and interfaces (#29)

--- src/DonateTo.ApplicationCore/Interfaces/IRepository.cs
+++ src/DonateTo.ApplicationCore/Interfaces/IRepository.cs
@@ -4,26 +4,77 @@ using DonateTo.ApplicationCore.Entities;
 
 namespace DonateTo.ApplicationCore.Interfaces
 {
+    /// <summary>
+    ///     Interface to implement the Repository Pattern.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity.</typeparam>
     public interface IRepository<TEntity> where TEntity : Entity
     {
+        /// <summary>
+        ///     Get a list of entities.
+        /// </summary>
+        /// <returns>IEnumerable of TEntity.</returns>
         IEnumerable<TEntity> Get();
         
+        /// <summary>
+        ///     Get a list of entities async.
+        /// </summary>
+        /// <returns>Task of IEnumerable of TEntity.</returns>
         Task<IEnumerable<TEntity>> GetAsync();
 
+        /// <summary>
+        ///     Get an entity by id.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <returns>TEntity.</returns>
         TEntity Get(long id);
 
+        /// <summary>
+        ///     Get an entity by id async.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <returns>Task of TEntity.</returns>
         Task<TEntity> GetAsync(long id);
 
+        /// <summary>
+        ///     Add an entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        /// <returns>Task of TEntity.</returns>
         TEntity Add(TEntity entity);
 
+        /// <summary>
+        ///     Add en entity async.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        /// <returns>TEntity.</returns>
         Task<TEntity> AddAsync(TEntity entity);
 
+        /// <summary>
+        ///     Update an entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        /// <returns>Task of TEntity.</returns>
         TEntity Update(TEntity entity);
 
+        /// <summary>
+        ///     Update an entity async.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        /// <returns>TEntity.</returns>
         Task<TEntity> UpdateAsync(TEntity entity);
 
+        /// <summary>
+        ///     Delete an entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
         void Delete(TEntity entity);
 
+        /// <summary>
+        ///     Delete an entity async.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        /// <returns>Task.</returns>
         Task DeleteAsync(TEntity entity);
     }
 }
--- src/DonateTo.WebApi/Middlewares/ExceptionHandlerMiddleware.cs
+++ src/DonateTo.WebApi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -6,6 +6,9 @@ using DonateTo.Infrastructure.Logging;
 
 namespace DonateTo.WebApi.Middlewares
 {
+    /// <summary>
+    ///     Add this middleware in order to capture any unhandled exception.
+    /// </summary>
     public class ExceptionHandlerMiddleware
     {
         private readonly RequestDelegate _next;
@@ -15,19 +18,29 @@ namespace DonateTo.WebApi.Middlewares
             _next = next;
         }
 
+        /// <summary>
+        ///     Processes the RequestDelegate.
+        /// </summary>
+        /// <param name="httpContext">HttpContext with all the 
+        /// information about the request.</param>
         public async Task InvokeAsync(HttpContext httpContext)
         {
             try
             {
-                await _next(httpContext);
+                await _next(httpContext).ConfigureAwait(false);
             }
             catch (Exception exc)
             {
                 Logger.Error("Unhandled Exception occurred",exc);
-                await HandleExceptionAsync(httpContext);
+                await HandleExceptionAsync(httpContext).ConfigureAwait(false);
             }
         }
 
+        /// <summary>
+        ///     Returns a 500 error with a custom message.
+        /// </summary>
+        /// <param name="httpContext">HttpContext with all the 
+        /// information about the request.</param>
         private Task HandleExceptionAsync(HttpContext context)
         {
             context.Response.ContentType = "application/json";
