480252e0c51dc8846948084d92190c8070f4374b
Nico Granata <nicolasegranata@gmail.com>
2020-04-28 15:30:49 -0300

    Adding summary to the existing methods and interfaces (#29)

--- src/DonateTo.ApplicationCore/Interfaces/IUnitOfWork.cs
+++ src/DonateTo.ApplicationCore/Interfaces/IUnitOfWork.cs
@@ -2,10 +2,21 @@
 
 namespace DonateTo.ApplicationCore.Interfaces
 {
+    /// <summary>
+    ///     Unit Of Work.
+    /// </summary>
     public interface IUnitOfWork
     {
+        /// <summary>
+        ///     Applies all database changes.
+        /// </summary>
+        /// <returns>Number of affected rows.</returns>
         Task<int> SaveAsync();
 
+        /// <summary>
+        ///     Applies all database changes.
+        /// </summary>
+        /// <returns>Number of affected rows.</returns>
         int Save();
     }
 }
--- src/DonateTo.Services/Extensions/ServiceCollectionExtension.cs
+++ src/DonateTo.Services/Extensions/ServiceCollectionExtension.cs
@@ -6,6 +6,11 @@ namespace DonateTo.Services.Extensions
 {
     public static class ServiceCollectionExtension
     {
+        /// <summary>
+        ///     Add all the DonateTo's dependencies to the collection.
+        /// </summary>
+        /// <param name="service">IServiceCollection.</param>
+        /// <param name="configuration">IConfiguration.</param>
         public static void AddDonateToModule(this IServiceCollection service, IConfiguration configuration)
         {
             service.AddEntityFramework(configuration);
