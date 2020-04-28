d30e973d5b9faaa73de4c774b74e810963efabe8
matigrieben <matigrieben@gmail.com>
2020-04-28 15:57:20 -0300

    Added pagination implementation (#23)
    * Added pagination implementation
    
    * Refactor and implement in repository
    
    * Refactor and add summary
    
    * Refactor
    
    Co-authored-by: Francisco Ghersi <francisco_ghersi@hotmail.com>

---
+++ src/DonateTo.ApplicationCore/Models/Pagination/PagedResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DonateTo.ApplicationCore.Models.Pagination
+{
+    public class PagedResult<T> where T : class
+    {
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+
+        public IEnumerable<T> Results { get; set; }
+
+        public int FirstRowOnPage
+        {
+            get { return (CurrentPage - 1) * PageSize + 1; }
+        }
+
+        public int LastRowOnPage
+        {
+            get { return Math.Min(CurrentPage * PageSize, RowCount); }
+        }
+
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
