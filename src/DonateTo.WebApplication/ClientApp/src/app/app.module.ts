ca80f43d1d006aab75c802bd7981d890401d547a
Facundo Lewis <facu.lewis@gmail.com>
2020-05-04 14:03:57 -0300

    Added prettier and husky. Fixed wrong code format (#46)

--- src/DonateTo.WebApplication/ClientApp/src/app/app.module.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/app.module.ts
@@ -1,35 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 // Angular core modules
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 // Routes
 import { AppRoutingModule } from './app-routing.module';
 
 // Modules
-import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AuthModule } from './auth/auth.module';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HomeComponent } from './home/home.component';
 
 // Third party libraries
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpErrorInterceptor } from 'src/app/shared/async-services/http/http-error.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
-  ],
+  declarations: [AppComponent, HomeComponent, CounterComponent, FetchDataComponent],
   imports: [
     AppRoutingModule,
     // Angular core dependencies
@@ -46,11 +41,9 @@ import { HttpErrorInterceptor } from 'src/app/shared/async-services/http/http-er
       { path: 'fetch-data', component: FetchDataComponent },
     ]),
     EffectsModule.forRoot([]),
-    AuthModule
+    AuthModule,
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/DonateTo.WebApplication/ClientApp/src/app/shared/async-services/error.service.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/shared/async-services/error.service.ts
@@ -2,26 +2,23 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable()
-
 export class ErrorService {
-    private errors = new Subject<string[]>();
+  private errors = new Subject<string[]>();
 
-    constructor() { }
+  constructor() {}
 
-    /**
+  /**
    * Adds a new value to the Subject
    *
    * @param errors - List of string
    * @returns void
    */
-    public addErrors = (errors: string[]): void =>
-        this.errors.next(errors)
+  public addErrors = (errors: string[]): void => this.errors.next(errors);
 
-    /**
+  /**
    *  Observable that the Subject casts to
    *
    * @returns Observable
    */
-    public getErrors = () =>
-        this.errors.asObservable()
+  public getErrors = () => this.errors.asObservable();
 }
