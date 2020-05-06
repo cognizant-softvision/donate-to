50b6a9d06c69aacad9b0b4432589ab63376b11a5
Karen Ono <onokarent@gmail.com>
2020-05-06 17:47:43 -0300

    Modified AppComponent

--- src/DonateTo.WebApplication/ClientApp/src/app/app.component.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
-import {TranslateService} from 'ng2-translate';
+import { TranslateService } from 'ng2-translate';
+import { AppSandbox } from './app.sandbox';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [AppSandbox],
 })
 export class AppComponent {
   title = 'app';
 
-  param = {value: 'donate'};
+  param = { value: 'donate' };
 
-    constructor(translate: TranslateService) {
-        // this language will be used as a fallback when a translation isn't found in the current language
-        translate.setDefaultLang('es');
+  constructor(translate: TranslateService, public appSandbox: AppSandbox) {
+    this.appSandbox.setupLanguage();
 
-         // the lang to use, if the lang isn't available, it will use the current loader to get them
-        translate.use('es');
-    }
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('es');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('es');
+  }
 }
