e8ace7efadf4a66ccbd3746e68739e04e381597b
Nico Granata <nicolasegranata@gmail.com>
2020-05-14 12:50:39 -0300

    Adding auth.guard (#73)

--- src/DonateTo.WebApplication/ClientApp/src/app/shared/store/auth/selectors.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/shared/store/auth/selectors.ts
@@ -8,6 +8,7 @@ export const getUserName = createSelector(getAuthState, (state: AuthState) => st
 export const getUserEmail = createSelector(getAuthState, (state: AuthState) => state.email);
 export const getAccessToken = createSelector(getAuthState, (state: AuthState) => state.access_token);
 export const isAuthenticated = createSelector(getAuthState, (state: AuthState) => state.isAuthenticated);
+export const isLoginProcessed = createSelector(getAuthState, (state: AuthState) => state.isLoginProcessed);
 
 @Injectable()
 export class AuthSelectors {
@@ -16,5 +17,6 @@ export class AuthSelectors {
   userName$ = this.store.select(getUserName);
   userEmail$ = this.store.select(getUserEmail);
   accessToken$ = this.store.select(getAccessToken);
-  isAuthenticated = this.store.select(isAuthenticated);
+  isAuthenticated$ = this.store.select(isAuthenticated);
+  isLoginProcessed$ = this.store.select(isLoginProcessed);
 }
