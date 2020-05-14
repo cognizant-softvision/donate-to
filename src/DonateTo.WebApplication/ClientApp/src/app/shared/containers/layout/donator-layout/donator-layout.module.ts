55540df4038ddb7101f22f42ff08f51c256d2a43
pablo-bergonzi <pablo.bergonzi@hotmail.com>
2020-05-14 12:44:45 -0300

    TranslateModule Fix

--- src/DonateTo.WebApplication/ClientApp/src/app/shared/containers/layout/donator-layout/donator-layout.module.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/shared/containers/layout/donator-layout/donator-layout.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { NzButtonModule, NzLayoutModule, NzMenuModule } from 'ng-zorro-antd';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { DonatorLayoutComponent } from './donator-layout.component';
 import { ComponentsModule } from 'src/app/shared/components';
 import { DonatorLayoutRoutingModule } from './donator-layout-routing.module';
 import { HomeModule } from 'src/app/home/home.module';
+import { HttpLoaderFactory } from 'src/app/app.module';
+import { HttpClient } from '@angular/common/http';
 
 export const COMPONENTS = [DonatorLayoutComponent];
 @NgModule({
@@ -20,7 +22,17 @@ export const COMPONENTS = [DonatorLayoutComponent];
     NzButtonModule,
     NzSpaceModule,
     NzMenuModule,
-    TranslateModule,
+    TranslateModule.forChild({
+      defaultLanguage: 'en',      
+     
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+       
+      },
+      extend: true,
+    }),
     ComponentsModule,
     HomeModule,
     DonatorLayoutRoutingModule
