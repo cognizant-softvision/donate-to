beeec8ec5832c1ba1ef9a4361e9b3a1c4ab28c42
Miguel B <miguell.beneditt@gmail.com>
2020-05-20 15:33:38 -0300

    Add Basic Page Model And Page Not Found (#119)
    * Add Not Found Page
    Add Basic Layout Module
    
    * Add Basic-Pages Module
    
    * Remove css text-align
    
    * Fix indentation
    
    * Remove element em , b
    
    * Fix bad translation
    
    * Fix Commit Request

---
+++ src/DonateTo.WebApplication/ClientApp/src/app/basic-pages/basic-pages.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { IconDefinition } from '@ant-design/icons-angular';
+
+import { FrownOutline } from '@ant-design/icons-angular/icons';
+
+import { NzButtonModule, NzIconModule, NzLayoutModule } from 'ng-zorro-antd';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BasicPagesRoutingModule } from './basic-pages-routing.module';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ComponentsModule } from '../shared/components';
+
+const ICONS: IconDefinition[] = [FrownOutline];
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../../../i18n/', '.json');
+}
+
+@NgModule({
+  imports: [
+    NzIconModule.forChild(ICONS),
+    NzLayoutModule,
+    ComponentsModule,
+    NzButtonModule,
+    BasicPagesRoutingModule,
+    TranslateModule.forChild({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      extend: true,
+    }),
+  ],
+  declarations: [NotFoundComponent],
+})
+export class BasicPagesModule {}
