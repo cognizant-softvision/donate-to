410e48ed27088a82b0cebffe8ca5003b811cd3eb
Guillermo Murano <guillermo.murano@gmail.com>
2020-05-21 11:27:30 -0300

    Capitalized normalized role names

--- src/DonateTo.Infrastructure/Data/Seed/Role.sql
+++ src/DonateTo.Infrastructure/Data/Seed/Role.sql
@@ -1,7 +1,7 @@
 INSERT INTO public."Role" ("Id", "Name", "NormalizedName", "ConcurrencyStamp", "Description", "CreatedBy", "CreatedDate", "UpdateBy", "UpdateDate")
 VALUES
-(1, 'Superadmin', 'Superadmin', NULL, 'All access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc')),
-(2, 'Admin', 'Administrator', NULL, 'Administrator access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc')),
-(3, 'Organization', 'Organization', NULL, 'Organization access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc')),
-(4, 'Donor', 'Donor', NULL, 'Donor access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc'))
+(1, 'Superadmin', 'SUPERADMIN', NULL, 'All access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc')),
+(2, 'Admin', 'ADMIN	', NULL, 'Administrator access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc')),
+(3, 'Organization', 'ORGANIZATION', NULL, 'Organization access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc')),
+(4, 'Donor', 'DONOR', NULL, 'Donor access rights', 'DataSeed', (SELECT now() at time zone 'utc'), 'DataSeed', (SELECT now() at time zone 'utc'))
 ON CONFLICT DO NOTHING;
