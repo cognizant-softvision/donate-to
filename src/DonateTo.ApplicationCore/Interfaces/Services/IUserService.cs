cef5556406b4ecf1ca2fc70322c0ef92a96402ed
Francisco Ghersi <francisco_ghersi@hotmail.com>
2020-05-21 16:39:11 -0300

    Add endpoint to get users with pagination

--- src/DonateTo.ApplicationCore/Interfaces/Services/IUserService.cs
+++ src/DonateTo.ApplicationCore/Interfaces/Services/IUserService.cs
@@ -1,8 +1,11 @@
 ﻿using DonateTo.ApplicationCore.Entities;
+using DonateTo.ApplicationCore.Models.Pagination;
+using System.Threading.Tasks;
 
 namespace DonateTo.ApplicationCore.Interfaces.Services
 {
     public interface IUserService : IBaseService<User>
     {
+        Task<PagedResult<User>> GetPagedUsersByOrganizationAsync(long organizationId, int page, int pageSize);
     }
 }
