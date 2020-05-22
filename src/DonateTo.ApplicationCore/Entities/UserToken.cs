16a46c5d11f4301bf9feffd4cf165ecc2d6544fd
Guillermo Murano <guillemurano@users.noreply.github.com>
2020-05-22 11:52:46 -0300

    Feature/add user to role donor (#134)
    * Add role to user on registry
    
    * Add identity relationships
    
    * Added identity custom entities, added new migration to fix tables and new dbContext in IdentityServer project to allow using IdentityDbContext base class avoiding reported framework bug https://github.com/dotnet/aspnetcore/issues/12511

---
+++ src/DonateTo.ApplicationCore/Entities/UserToken.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DonateTo.ApplicationCore.Entities
+{
+    public class UserToken : IdentityUserToken<long>
+    {
+        public virtual User User { get; set; }
+    }
+}
