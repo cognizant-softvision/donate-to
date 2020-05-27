295b4d4abda5bd68ddc170f356765d8e73a26178
Matias Grieben <matigrieben@gmail.com>
2020-05-27 13:11:03 -0300

    Replace baseUrl port WIP

--- src/DonateTo.WebApplication/ClientApp/src/environments/environment.ts
+++ src/DonateTo.WebApplication/ClientApp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:44328',
+  baseUrl: 'https://localhost:5001',
   paths: {
     imagesRoot: '/src/assets/images/',
   },
