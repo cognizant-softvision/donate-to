0fc3a96d6c589e257bcdb44aa12dbde95242cb21
Guillermo Murano <guillermo.murano@gmail.com>
2020-05-29 19:28:22 -0300

    Added mail sender project, modified registration cycle to validate email and forgot password

---
+++ DonateTo.Mailer/Entities/MailAddress.cs
@@ -0,0 +1,17 @@
+﻿using MimeKit;
+
+namespace DonateTo.Mailer.Entities
+{
+    public class MailAddress : MailboxAddress
+    {
+        ///<inheritdoc cref="MailboxAddress"/>
+        public MailAddress(string address) : base(address)
+        {
+        }
+
+        ///<inheritdoc cref="MailboxAddress"/>
+        public MailAddress(string alias, string address) : base(alias, address)
+        {
+        }
+    }
+}
