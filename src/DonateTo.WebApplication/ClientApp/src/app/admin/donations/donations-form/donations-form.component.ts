474789a62deb7dcc499ac0249c54f6b5bb6180a3
Miguel Benedit <miguell.beneditt@gmail.com>
2020-06-03 13:04:02 -0300

    Add donations-form

---
+++ src/DonateTo.WebApplication/ClientApp/src/app/admin/donations/donations-form/donations-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donations-form',
+  templateUrl: './donations-form.component.html',
+  styleUrls: ['./donations-form.component.css'],
+})
+export class DonationsFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
