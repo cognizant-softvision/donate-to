08ef48e6d29fd72f38a0fdd1b00a3425bf55499e
pablo-bergonzi <pablo.bergonzi@hotmail.com>
2020-06-08 16:42:43 -0300

    PR Fix

--- src/DonateTo.IdentityServer/Services/ProfileService.cs
+++ src/DonateTo.IdentityServer/Services/ProfileService.cs
@@ -19,6 +19,11 @@ namespace DonateTo.IdentityServer.Services
             _userManager = userManager;
         }
 
+        /// <summary>
+        /// Overridden method that gets call while get profile data
+        /// </summary>
+        /// <param name="context">profile context</param>
+        /// <returns></returns>
         public async Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
             var user = await _userManager.GetUserAsync(context.Subject);
@@ -34,6 +39,11 @@ namespace DonateTo.IdentityServer.Services
             context.IssuedClaims.AddRange(claims);
         }
 
+        /// <summary>
+        /// Overridden method that gets call while checking if profile is active
+        /// </summary>
+        /// <param name="context">profile active context</param>
+        /// <returns></returns>
         public async Task IsActiveAsync(IsActiveContext context)
         {
             var user = await _userManager.GetUserAsync(context.Subject);
