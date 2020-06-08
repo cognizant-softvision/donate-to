7a2eef0607af881e2bebdca414f35b31701630f7
Francisco Ghersi <francisco_ghersi@hotmail.com>
2020-06-08 10:57:10 -0300

    add onChenge

--- src/DonateTo.WebApplication/ClientApp/src/app/home/home.component.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,9 @@ import { HomeSandbox } from './home.sandbox';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { ConfigService } from '../app-config.service';
+import { fromEvent, Subject } from 'rxjs';
+import { map } from 'rxjs-compat/operator/map';
+import { debounceTime, distinctUntilChanged, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -24,10 +27,23 @@ export class HomeComponent implements OnInit {
   }
 
   subscriptions: Subscription[] = [];
+  inputChange = new Subject();
 
   ngOnInit(): void {}
 
   onSearch() {
+    this.homeSandbox.loadDonationRequestsSearchPaged(this.pageSize, this.currentPage, this.searchValue);
+  }
+
+  onChange(e) {
+    this.inputChange.next(e);
+    const valueSearch$ = fromEvent(e, 'input').pipe(
+      map((event) => event.target.value),
+      filter((query) => query),
+      debounceTime(1000),
+      distinctUntilChanged(),
+      tap((query) => console.log(`About to make an API call with query: ${query}`))
+    );
     this.homeSandbox.loadDonationRequestsSearchPaged(this.pageSize, this.currentPage, this.searchValue);
   }
 
