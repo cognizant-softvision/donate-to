bfa5a850d46ad689237644c680653dc6eedd1b6e
Guillermo Murano <guillemurano@users.noreply.github.com>
2020-06-09 12:05:22 -0300

    Hotfix/show donation request cards and fix models (#159)
    * Fix error on donation request list, fix model to support info from database
    
    * Added models to index file

--- src/DonateTo.ApplicationCore/Entities/DonationRequestItem.cs
+++ src/DonateTo.ApplicationCore/Entities/DonationRequestItem.cs
@@ -8,7 +8,8 @@ namespace DonateTo.ApplicationCore.Entities
         public string Observation { get; set; }
         public decimal CurrentQuantity { get; set; }
         public decimal FinishQuantity { get; set; }         
-        public string Unit { get; set; }       
+        public long UnitId { get; set; }
+        public Unit Unit { get; set; }
         public IEnumerable<DonationRequestItemCategory> DonationRequestItemCategories { get; set; }  
     }
 }
