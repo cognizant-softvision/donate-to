dfd5d4eec6e395696002bf748ad2b912241b40bf
Guillermo Murano <guillermo.murano@gmail.com>
2020-06-10 01:15:50 -0300

    Remove unneeded interface

--- src/DonateTo.Infrastructure/Data/Repositories/DonationItemRepository.cs
+++ src/DonateTo.Infrastructure/Data/Repositories/DonationItemRepository.cs
@@ -1,26 +1,12 @@
 ﻿using DonateTo.ApplicationCore.Entities;
-using DonateTo.ApplicationCore.Interfaces;
 using DonateTo.Infrastructure.Data.EntityFramework;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace DonateTo.Infrastructure.Data.Repositories
 {
-    public class DonationItemRepository : EntityFrameworkRepository<DonationItem, DonateToDbContext>, IDonationItemRepository
+    public class DonationItemRepository : EntityFrameworkRepository<DonationItem, DonateToDbContext>
     {
-        private readonly DonateToDbContext _dbContext;
-
         public DonationItemRepository(DonateToDbContext dbContext) : base(dbContext)
         {
-            _dbContext = dbContext;
-        }
-
-        ///<inheritdoc cref="IDonationItemRepository"/>
-        public async Task<IEnumerable<DonationItem>> AddAllAsync(IEnumerable<DonationItem> donationItems)
-        {
-            await _dbContext.Set<DonationItem>().AddRangeAsync(donationItems).ConfigureAwait(false);
-
-            return donationItems;
         }
     }
 }
