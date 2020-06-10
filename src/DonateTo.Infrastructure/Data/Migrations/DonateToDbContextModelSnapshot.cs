6d5d89862786ce60d68431407933632b29ba5ed9
Guillermo Murano <guillemurano@users.noreply.github.com>
2020-06-10 17:55:17 -0300

    Hotfix/change migration constraint (#167)
    * Revert "Hotfix/add unit id to request (#166)"
    
    This reverts commit a2ccd6f455f320b5a23142a1a7399129ee9d393b.
    
    * Revert "Add migration (#164)"
    
    This reverts commit cbf43ac49c128c92d71052663d8d375d6b918d82.
    
    * Fixed migrations

--- src/DonateTo.Infrastructure/Data/Migrations/DonateToDbContextModelSnapshot.cs
+++ src/DonateTo.Infrastructure/Data/Migrations/DonateToDbContextModelSnapshot.cs
@@ -334,7 +334,7 @@ namespace DonateTo.Infrastructure.Migrations
                     b.Property<long>("StatusId")
                         .HasColumnType("bigint");
 
-                    b.Property<long>("UnitId")
+                    b.Property<long?>("UnitId")
                         .HasColumnType("bigint");
 
                     b.Property<string>("UpdateBy")
@@ -955,9 +955,7 @@ namespace DonateTo.Infrastructure.Migrations
 
                     b.HasOne("DonateTo.ApplicationCore.Entities.Unit", "Unit")
                         .WithMany()
-                        .HasForeignKey("UnitId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                        .HasForeignKey("UnitId");
                 });
 
             modelBuilder.Entity("DonateTo.ApplicationCore.Entities.DonationRequest", b =>
