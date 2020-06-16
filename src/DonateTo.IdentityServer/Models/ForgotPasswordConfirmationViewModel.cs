288efb522143bc68d5c0fa7a96dc8d36c75813aa
Francisco Ghersi <francisco_ghersi@hotmail.com>
2020-06-16 12:42:32 -0300

    Add forgotPasswordConfirmationViewModel

---
+++ src/DonateTo.IdentityServer/Models/ForgotPasswordConfirmationViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DonateTo.IdentityServer.Models
+{
+    public class ForgotPasswordConfirmationViewModel
+    {
+        public string RedirectUris { get; set; } = "http://localhost:4200";
+    }
+}
