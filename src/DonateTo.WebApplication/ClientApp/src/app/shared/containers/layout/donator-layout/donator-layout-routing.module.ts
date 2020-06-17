d980edaccf6af683cf03f32bf153a1d0ffd43fcf
matigrieben <matigrieben@gmail.com>
2020-06-17 11:40:14 -0300

    bug resolved (#198)

--- src/DonateTo.WebApplication/ClientApp/src/app/shared/containers/layout/donator-layout/donator-layout-routing.module.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/shared/containers/layout/donator-layout/donator-layout-routing.module.ts
@@ -2,16 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from '../../../../home/home.component';
 import { DonatorLayoutComponent } from './donator-layout.component';
-import { DonationComponent } from 'src/app/donation/donation.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DonatorLayoutComponent,
-    children: [
-      { path: '', component: HomeComponent },
-      { path: 'donation/:donationRequestId', component: DonationComponent },
-    ],
+    children: [{ path: '', component: HomeComponent }],
   },
 ];
 
