7fc5fe43d52c7055e160d323bcfb0c625edd87b8
Matias Grieben <matigrieben@gmail.com>
2020-06-17 17:21:14 -0300

    WIP

--- src/DonateTo.IdentityServer/Services/ProfileService.cs
+++ src/DonateTo.IdentityServer/Services/ProfileService.cs
@@ -35,6 +35,7 @@ namespace DonateTo.IdentityServer.Services
             var claims = principal.Claims.ToList();
             claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
             claims.Add(new Claim("user_Id", user.Id.ToString() ?? string.Empty));
+            claims.Add(new Claim("user_Email", user.Email.ToString() ?? string.Empty));
 
             var roleClaimName = JwtClaimTypes.Role;
             var existingRoles = context.IssuedClaims.Where(c => c.Type == roleClaimName).Select(c => c.Value);
