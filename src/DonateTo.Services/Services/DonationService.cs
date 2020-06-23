25fa6e834b5f837c60b0d675181de0ad9b6d8b8f
Guillermo Murano <guillemurano@users.noreply.github.com>
2020-06-23 12:05:08 -0300

    Feature/email sending (#216)
    * Added mail to donation request
    
    * Added Mail to donation request creation
    
    * Fixed merge errors
    
    * Fixed call to mailer
    
    * Added Less Compilation To The Build Pipeline (#211)
    
    * Merged
    
    * Added reference
    
    * Added mail service to new donation
    
    * Add missing interface
    
    * fixed Gulp version
    
    * Added summary from interface
    
    Co-authored-by: pablo-bergonzi <pablo.bergonzi@hotmail.com>

--- src/DonateTo.Services/Services/DonationService.cs
+++ src/DonateTo.Services/Services/DonationService.cs
@@ -1,14 +1,48 @@
 ﻿using DonateTo.ApplicationCore.Entities;
 using DonateTo.ApplicationCore.Interfaces;
+using DonateTo.ApplicationCore.Interfaces.Services;
+using DonateTo.Mailer.Entities;
+using DonateTo.Mailer.Interfaces;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
 
 namespace DonateTo.Services
 {
-    public class DonationService: BaseService<Donation>
+    public class DonationService: BaseService<Donation>, IDonationService
     {
+        private readonly IMailSender _mailSender;
+
         public DonationService(
+            IMailSender mailSender,
             IRepository<Donation> donationRepository,
             IUnitOfWork unitOfWork) : base(donationRepository, unitOfWork)
         {
+            _mailSender = mailSender;
+        }
+
+        ///<inheritdoc cref="IDonationService"/>
+        public async Task SendNewDonationMailAsync(Donation donation, User user, string client)
+        {
+            var body = @"<p>Hi {0}!</p>
+                            <p>Thanks for your recent Donation.</p>
+                            <p>Check it <a href='{1}'>here</a></p>";
+
+            var bodyMessage = new MessageBody()
+            {
+                HtmlBody = string.Format(CultureInfo.InvariantCulture, body,
+                                            user.FullName,
+                                            client)
+            };
+
+            var to = new List<string>
+            {
+                user.Email
+            };
+
+            var message = new Message(to, "You've made a new donation!", bodyMessage);
+
+            await _mailSender.SendAsync(message).ConfigureAwait(false);
         }
     }
 }
