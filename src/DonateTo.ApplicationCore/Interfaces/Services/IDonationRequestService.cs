6af6fc1893a781baee0568bd03059f15469db1d0
Guillermo Murano <guillemurano@users.noreply.github.com>
2020-06-23 12:05:08 -0300

    Feature/email sending (#216)
    * Added mail to donation request
    
    * Added Mail to donation request creation
    
    * Fixed merge errors
    
    * Fixed call to mailer
    
    * Added Less Compilation To The Build Pipeline (#211)
    
    * Merged
    
    * Added reference
    
    * Added mail service to new donation
    
    * Add missing interface
    
    * fixed Gulp version
    
    * Added summary from interface
    
    Co-authored-by: pablo-bergonzi <pablo.bergonzi@hotmail.com>

---
+++ src/DonateTo.ApplicationCore/Interfaces/Services/IDonationRequestService.cs
@@ -0,0 +1,18 @@
+﻿using DonateTo.ApplicationCore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DonateTo.ApplicationCore.Interfaces.Services
+{
+    public interface IDonationRequestService : IBaseService<DonationRequest>
+    {
+        /// <summary>
+        /// Send Created request info mail to all users of the organization
+        /// </summary>
+        /// <param name="donationRequest">DonationRequest</param>
+        /// <param name="users">IEnumerable<Users></Users></param>
+        /// <param name="client">Client</param>
+        /// <returns></returns>
+        Task SendNewRequestMailToOrganizationUsersAsync(DonationRequest donationRequest, IEnumerable<User> users, string client);
+    }
+}
