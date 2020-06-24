d66ef34877741a587e718d9081b3647b97e61842
Karen Ono <onokarent@gmail.com>
2020-06-24 12:36:32 -0300

    New components

---
+++ src/DonateTo.WebApplication/ClientApp/src/app/admin/user/components/users-list/users-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css'],
+})
+export class UsersListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
