d4eccda001aa16f60a52acdcb2335a9e7e557224
matigrieben <matigrieben@gmail.com>
2020-07-06 12:45:04 -0300

    Bugfix/donor user admin page (#289)
    * Add admin button
    
    * Add superadmin role
    
    * Add admin auth
    
    * Bug fixed
    
    * Comments resolved
    
    * Bug resolved
    
    Co-authored-by: Francisco Ghersi <francisco_ghersi@hotmail.com>

--- src/DonateTo.WebApplication/ClientApp/src/app/shared/auth/auth.sandbox.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/shared/auth/auth.sandbox.ts
@@ -4,15 +4,36 @@ import { OAuthEvent, OAuthService } from 'angular-oauth2-oidc';
 import { Sandbox } from '../sandbox/base.sandbox';
 import { Store } from '@ngrx/store';
 import * as store from '../store';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { Roles } from '../enum/roles';
 
 @Injectable()
 export class AuthSandbox extends Sandbox {
+  private subscriptions: Subscription[] = [];
+  public isAdmin = new BehaviorSubject(false);
+
   constructor(
     protected appState$: Store<store.State>,
     private authService: OAuthService,
     private authConfigService: AuthConfigService
   ) {
     super(appState$);
+    this.registerEvents();
+  }
+
+  /**
+   * Subscribes to events
+   */
+  private registerEvents(): void {
+    this.subscriptions.push(
+      this.userRoles$.subscribe((userRoles: string[]) =>
+        this.isAdmin.next(
+          userRoles.includes(Roles.Admin) ||
+            userRoles.includes(Roles.Superadmin) ||
+            userRoles.includes(Roles.Organization)
+        )
+      )
+    );
   }
 
   /**
