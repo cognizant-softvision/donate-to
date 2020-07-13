055bcf283a44b8c315374dc582ec09f221fc4db1
Karen <31966611+oonosan@users.noreply.github.com>
2020-07-13 09:46:37 -0300

    Changed my profile view (#306)

--- src/DonateTo.WebApplication/ClientApp/src/app/home/my-profile/my-profile-layout.module.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/home/my-profile/my-profile-layout.module.ts
@@ -3,6 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NzButtonModule, NzInputModule, NzLayoutModule, NzTableModule } from 'ng-zorro-antd';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { EditTwoTone, SettingTwoTone, SmileTwoTone } from '@ant-design/icons-angular/icons';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpLoaderFactory } from 'src/app/basic-pages/basic-pages.module';
@@ -11,6 +16,7 @@ import { MyProfileComponent } from './my-profile.component';
 import { ProfileLayoutRoutingModule } from './my-profile-layout-routing.module';
 import { ComponentsModule } from 'src/app/shared/components';
 
+const icons = [SmileTwoTone, SettingTwoTone, EditTwoTone];
 @NgModule({
   imports: [
     FormsModule,
@@ -20,6 +26,10 @@ import { ComponentsModule } from 'src/app/shared/components';
     NzTableModule,
     NzSpaceModule,
     NzInputModule,
+    NzDescriptionsModule,
+    NzPageHeaderModule,
+    NzTabsModule,
+    NzIconModule.forChild(icons),
     ProfileLayoutRoutingModule,
     TranslateModule.forChild({
       defaultLanguage: 'en',
