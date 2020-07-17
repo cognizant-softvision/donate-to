1980feda895dc2c2f8a6d50eddaca77988900ed7
Matias Grieben <matigrieben@gmail.com>
2020-07-17 12:55:21 -0300

    Implemented question service bulkupdate sync

--- src/DonateTo.Services/Services/QuestionService.cs
+++ src/DonateTo.Services/Services/QuestionService.cs
@@ -66,7 +66,7 @@ namespace DonateTo.Services
 
         public void BulkUpdate(IEnumerable<Question> updatedQuestions)
         {
-            throw new NotImplementedException();
+            _questionRepository.BulkUpdate(updatedQuestions);
         }
     }
 }
