2d624906c238014dbca0e2c960596469455accf2
Karen Ono <onokarent@gmail.com>
2020-07-28 12:51:41 -0300

    Applied UX suggested changes for Admin-Donations section

---
+++ src/DonateTo.WebApplication/ClientApp/src/app/admin/donations/donations-detail/donations-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donations-detail',
+  templateUrl: './donations-detail.component.html',
+  styleUrls: ['./donations-detail.component.css'],
+})
+export class DonationsDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
