7253168d41e358fbc851ac86725bdeefb8e00ca2
Miguel Benedit <miguell.beneditt@gmail.com>
2020-08-03 10:33:49 -0300

    Add Log Detail modal

---
+++ src/DonateTo.WebApplication/ClientApp/src/app/admin/log/log-detail-modal/log-detail-modal/log-detail-modal.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { LogModel } from 'src/app/shared/models/log.model';
+
+@Component({
+  selector: 'app-log-detail-modal',
+  templateUrl: './log-detail-modal.component.html',
+  styleUrls: ['./log-detail-modal.component.css'],
+})
+export class LogDetailModalComponent implements OnInit {
+  log: LogModel;
+  isVisible = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  showModal(log: LogModel) {
+    this.log = log;
+    this.isVisible = !this.isVisible;
+  }
+
+  closeModal() {
+    this.isVisible = !this.isVisible;
+  }
+}
