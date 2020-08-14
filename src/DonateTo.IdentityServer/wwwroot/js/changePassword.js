2a8c4e86816a4cd398e1f97061017c22d99c58be
Matias Grieben <matigrieben@gmail.com>
2020-08-14 12:45:26 -0300

    Added new page for Change password

---
+++ src/DonateTo.IdentityServer/wwwroot/js/changePassword.js
@@ -0,0 +1,25 @@
+﻿function CheckPasswordCriteria(passwordValue) {
+  const passwordElement = document.getElementById("newPassword");
+  const passwordRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/);
+
+  if (passwordRegex.test(passwordValue)) {
+    passwordElement.classList.remove("is-invalid");
+    passwordElement.classList.add("is-valid");
+  } else {
+    passwordElement.classList.remove("is-valid");
+    passwordElement.classList.add("is-invalid");
+  }
+}
+
+function CheckPasswordMatch(confirmPasswordValue) {
+  const passwordValue = document.getElementById("newPassword").value;
+  const confirmPasswordElement = document.getElementById("confirmNewPassword");
+
+  if (confirmPasswordValue === passwordValue) {
+    confirmPasswordElement.classList.remove("is-invalid");
+    confirmPasswordElement.classList.add("is-valid");
+  } else {
+    confirmPasswordElement.classList.remove("is-valid");
+    confirmPasswordElement.classList.add("is-invalid");
+  }
+}
