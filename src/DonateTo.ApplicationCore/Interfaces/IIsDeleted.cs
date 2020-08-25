634d6be17b5bbcb72f9253a11a06d1f7f1a5c2fe
Karen Ono <onokarent@gmail.com>
2020-08-25 16:14:09 -0300

    IsDeleted interface

---
+++ src/DonateTo.ApplicationCore/Interfaces/IIsDeleted.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DonateTo.ApplicationCore.Interfaces
+{
+    public interface IIsDeleted
+    {
+        bool IsDeleted { get; set; }
+    }
+}
