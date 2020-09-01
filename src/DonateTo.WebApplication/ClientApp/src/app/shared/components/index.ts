410ee9aca01fb096ce1d9def152debaabc006f8a
Karen Ono <onokarent@gmail.com>
2020-09-01 11:04:00 -0300

    Dropdown menu

--- src/DonateTo.WebApplication/ClientApp/src/app/shared/components/index.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/shared/components/index.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
-import { NzButtonModule, NzLayoutModule, NzMenuModule, NzRadioModule } from 'ng-zorro-antd';
+import { NzButtonModule, NzIconModule, NzLayoutModule, NzMenuModule, NzRadioModule } from 'ng-zorro-antd';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { CaretDownFill } from '@ant-design/icons-angular/icons';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { NavMenuSandBox } from './nav-menu/nav-menu.sandbox';
 import { RouterModule } from '@angular/router';
 
 export const COMPONENTS = [NavMenuComponent];
+const ICONS: IconDefinition[] = [CaretDownFill];
+
 @NgModule({
   imports: [
     FormsModule,
@@ -24,6 +28,7 @@ export const COMPONENTS = [NavMenuComponent];
     RouterModule,
     NzRadioModule,
     NzDropDownModule,
+    NzIconModule.forChild(ICONS),
   ],
   declarations: COMPONENTS,
   exports: COMPONENTS,
