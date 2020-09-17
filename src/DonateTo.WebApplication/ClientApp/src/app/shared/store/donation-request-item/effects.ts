00ecc3ae4985cb5a261c19767a0ac87450b94487
Karen <31966611+oonosan@users.noreply.github.com>
2020-09-16 21:06:33 -0300

    Update donation items (#459)

--- src/DonateTo.WebApplication/ClientApp/src/app/shared/store/donation-request-item/effects.ts
+++ src/DonateTo.WebApplication/ClientApp/src/app/shared/store/donation-request-item/effects.ts
@@ -1,5 +1,5 @@
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, switchMap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { DonationRequestItemService } from '../../async-services/http/donation-request-item.service';
@@ -8,13 +8,14 @@ import {
   removeDonationRequestItemFailed,
   removeDonationRequestItemSuccess,
 } from './actions';
+import { mergeMapTo } from 'rxjs-compat/operator/mergeMapTo';
 
 @Injectable()
 export class DonationRequestItemEffects {
   @Effect()
   removeDonationRequestItem$: Observable<{}> = this.actions$.pipe(
     ofType(removeDonationRequestItem),
-    switchMap((data: any) =>
+    mergeMap((data: any) =>
       this.donationRequestItemService.deleteDonationRequestItem(data.donationRequestItem).pipe(
         map((donationRequestItem) => removeDonationRequestItemSuccess({ donationRequestItem })),
         catchError(() => of(removeDonationRequestItemFailed()))
